9789bae5e526a27dea8273c157cfb7025d4c88a6
Nuray Çelik <viola.armoni@gmail.com>
2020-12-12 16:00:03 +0300

    Update meals_service.dart

--- lib/service/meals_service.dart
+++ lib/service/meals_service.dart
@@ -4,14 +4,14 @@ import 'package:http/http.dart' as http;
 import '../models/meal.dart';
 
 class MealService {
-  static const ADD_URL = "http://nc.nuraycelik.com/mymeals/add.php";
+  static const ADD_URL = "http://n.com/mymeals/add.php";
   static const VIEW_URL =
-      "http://nc.nuraycelik.com/contentmeals/contentviews.php";
+      "http://n.com/contentmeals/contentviews.php";
   static const GETID_URL =
-      "http://nc.nuraycelik.com/contentmeals/selectcatId.php";
-  static const UPDATE_URL = "http://nc.nuraycelik.com/mymeals/update.php";
+      "http://n.com/contentmeals/selectcatId.php";
+  static const UPDATE_URL = "http://n.com/mymeals/update.php";
   static const DELETE_URL =
-      "http://nc.nuraycelik.com/contentmeals/selectdelete.php";
+      "http://n.com/contentmeals/selectdelete.php";
 
   Future<String> addMeals(Meals meals) async {
     final response = await http.post(ADD_URL, body: meals.toJsonAdd());
@@ -42,7 +42,7 @@ class MealService {
   
  Future<List> getMealsId(String mealId) async {
     final response = await http.post(
-      "http://nc.nuraycelik.com/contentmeals/selectmealId.php",
+      "http://n.com/contentmeals/selectmealId.php",
       body: <String, String>{
         "id": mealId,
       },
@@ -62,7 +62,7 @@ class MealService {
   
     Future<List> getMealsCatId(String categoryId) async {
     final response = await http.post(
-      "http://nc.nuraycelik.com/contentmeals/selectcatId.php",
+      "http://n.com/contentmeals/selectcatId.php",
       body: <String, String>{
         "catId": categoryId,
       },
@@ -91,7 +91,7 @@ class MealService {
   
   //Bir sutundaki verinin değerini silme. 
   Future<String> updateMealsFavorite(String mealId, String isFavorite) async {
-    final response = await http.post("http://nc.nuraycelik.com/contentmeals/mealschange.php", body: <String, String>{
+    final response = await http.post("http://n.com/contentmeals/mealschange.php", body: <String, String>{
         "id": mealId,
         "isFavorite": isFavorite,
       },
